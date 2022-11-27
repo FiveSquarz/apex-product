bce3053014aee90e59147bf8b9399c76f1ab149e
FiveSquarz <82244309+FiveSquarz@users.noreply.github.com>
2022-11-26 20:36:08 -0500

    Remove default files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Game extends React.Component {
+  render() {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div className="game" style={{ fontSize: "5vmin" }}>
+          Chuanhai's APEX product will be here.. soon
+        </div>
+        {/*<div className="game" style={{ fontSize: "3vmin" }}>
+          Time Remaining: 5
+        </div>
+        <div className="game">
+          3
+        </div>
+        <div className="game">
+          <button onClick={() => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+          }} style={{ fontSize: "5vmin" }}>Start New Game</button>
+        </div>*/}
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
